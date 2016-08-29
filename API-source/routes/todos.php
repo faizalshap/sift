593a3cc4d3e04753e8d73b1166f9cdc69af47a78
John Correlli <john@teamgantt.com>
2016-08-29 16:05:41 -0400

    wip

--- API-source/routes/todos.php
+++ API-source/routes/todos.php
@@ -56,25 +56,28 @@ Route::is('put', 'API/todolists/{$todolist_id}/todos/{$todo_id}', function($todo
   /* IF TEAMGANTT TODOLIST - UPDATE TOLIST_ID */
   if($original_todolist_id == 'teamgantt') {
     $todolist_id = TeamGantt::fetch_teamgantt_todolist_id($logged_in_user);
-    $json['todolist_id'] = $todolist_id; //IF IT's A TEAMGANTT TASK - WE CAN'T MOVE IT
+    $teamgantt_todolist_id = $todolist_id; //IF IT's A TEAMGANTT TASK - WE CAN'T MOVE IT
+    $todo_id = NULL;
   }
+  else {
+    /* LOAD ORIGINAL TODO - SO WE KNOW WE HAVE ACCESS TO IT */
+    $original_todo = Todo::fetch($logged_in_user, $todo_id);
+    if(!$original_todo || $original_todo->todolist_id != $todolist_id) {
+      response_code(404);
+    }
 
-  /* LOAD ORIGINAL TODO - SO WE KNOW WE HAVE ACCESS TO IT */
-  $original_todo = Todo::fetch($logged_in_user, $todo_id);
-  if(!$original_todo || $original_todo->todolist_id != $todolist_id) {
-    response_code(404);
+    //MAKE SURE WE CAN POST INTO TODOLIST
+    $todolist = TodoList::fetch($logged_in_user, $json['todolist_id']);
+    if(!$todolist) {
+      response_code(404);
+    }
   }
 
   /* GET INFO FROM JSON */
   $json = fetch_json();
   $json['id'] = $todo_id;
-  $json['todolist_id'] = $json['todolist_id'] ?? $todolist_id;
-
-  //MAKE SURE WE CAN POST INTO TODOLIST
-  $todolist = TodoList::fetch($logged_in_user, $json['todolist_id']);
-  if(!$todolist) {
-    response_code(404);
-  }
+  $json['todolist_id'] = $teamgantt_todolist_id ?? $json['todolist_id'] ?? $todolist_id;
+  $json['created_at'] = gmdate("Y-m-d H:i:s");
 
   //SETUP NEW TODO
   $todo = new Todo($json);
@@ -90,7 +93,7 @@ Route::is('put', 'API/todolists/{$todolist_id}/todos/{$todo_id}', function($todo
   echo json_encode($todo);
 
   //IF TEAMGANTT - WE NEED TO UPDATE IT
-  if($original_todolist_id == 'teamgantt') {
+  if($json['teamgantt_id'] != '' && $original_todolist_id == TeamGantt::fetch_teamgantt_todolist_id($logged_in_user)) {
     TeamGantt::patch_task($logged_in_user, $todo);
   }
 });
